8d4ee7ed6f923f85e16aeb048ee54dbfe43f09f9
AlessandroBoco <35563145+AlessandroBoco@users.noreply.github.com>
2018-02-22 12:41:31 +0100

    Add files via upload

---
+++ Connessione.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+/**
+ *
+ * @author Studenti
+ */
+public class Connessione {
+     String mex;
+    String committ;
+    
+    public Connessione(String mex, String commit){
+        this.mex= mex;
+        this.committ= commit;
+    }
+     public static void main(String[] args) {
+         //server
+         
+    }
+}
